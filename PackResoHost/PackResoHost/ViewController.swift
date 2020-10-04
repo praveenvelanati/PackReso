93074e40fd28b0bfbd9b6bd9698ceb1ebef1bb0d
Praveen <praveen.velanati@sparity.com>
2020-10-04 11:18:50 -0400

    Added host app

---
+++ PackResoHost/PackResoHost/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PackResoHost
+//
+//  Created by praveen velanati on 10/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
